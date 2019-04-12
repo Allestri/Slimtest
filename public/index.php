7217a76b828917657faba8750d87520fda28fbd1
SOku <alexandre.pellae@gmail.com>
2019-04-12 22:37:39 +0200

    Modif test index

--- public/index.php
+++ public/index.php
@@ -2,10 +2,23 @@
 
 require '../vendor/autoload.php';
 
+
+class DemoMiddleware {
+    
+    
+    public function __invoke(\Slim\Http\Request $request, \Slim\Http\Response $response, $next){
+        $response->write('<h1>Bienvenue</h1>');
+        return $next($request, $response);
+    }
+    
+}
+
 $app = new \Slim\App();
+$app->add(new DemoMiddleware());
 $app->get('/salut/{nom}', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
     var_dump($args);
-    return $response->write('Bonjour tout le monde !');
+    $nom = "Marc";
+    return $response->write('Bonjour tout le monde ! ' . $nom);
 });
 
 $app->run();
