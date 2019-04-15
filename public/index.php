4e1079406d3b974098c7b4fefb2056f397dc120f
SOku <alexandre.pellae@gmail.com>
2019-04-15 18:38:36 +0200

    Utilisation bdd

--- public/index.php
+++ public/index.php
@@ -3,21 +3,34 @@
 require '../vendor/autoload.php';
 
 
+$pdo = new PDO('mysql:dbname=slimtest;host=localhost', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 class DemoMiddleware {
     
     
     public function __invoke(\Slim\Http\Request $request, \Slim\Http\Response $response, $next){
         $response->write('<h1>Bienvenue</h1>');
-        return $next($request, $response);
+        $respone = $next($request, $response);
+        $response->write('<h1>Au revoir</h1>');
+        return $response;
     }
     
 }
 
 $app = new \Slim\App();
 $app->add(new DemoMiddleware());
-$app->get('/salut/{nom}', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
+$app->get('/', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
     var_dump($args);
     $nom = "Marc";
+    
+    // Test request
+    $req = $pdo->prepare('SELECT * FROM posts');
+    $req->execute();
+    $posts = $req->fetchAll();
+    var_dump($posts);
+    
     return $response->write('Bonjour tout le monde ! ' . $nom);
 });
 
